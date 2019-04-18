707baf83b4d81a4e55cc03902e2f707bdc903092
Michael DiBlasio <mdiblasio@google.com>
2019-04-18 16:51:29 -0400

    Revert "Revert "Revert "major updates"""
    This reverts commit ee4bbe73d63cece71fb820d2082a27b90790ca2a.

--- steps/workbox-config_step_final.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-module.exports = {
-  swSrc: path.join('src', 'service-worker.js'),
-  swDest: path.join('dist', 'service-worker.js'),
-  globDirectory: './public/',
-  globPatterns: [
-    '*.{html,js,css}'
-  ]
-};
